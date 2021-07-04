b068da00abfd3b64ccdadf34b5d55c5849b859a4
Tushar_Maity <tusharmaity989@gmail.com>
2021-07-04 10:47:16 +0530

    Basic navbar dummy

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+        <Navbar />
+        <Switch>
+            <Route path="/" />
+        </Switch>
+    </Router>
   );
 }
 
